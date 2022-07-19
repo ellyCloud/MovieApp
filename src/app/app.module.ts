c63f9055ec66a3015fbdd17075d9bc6d9b9c3260
Elif Sude Bulut <c1928007@student.cankaya.edu.tr>
2022-07-19 16:51:52 +0200

    2-Generating New Component Using CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesComponent],
+  declarations: [AppComponent, MoviesComponent, MovieComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css'],
+})
+export class MovieComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
