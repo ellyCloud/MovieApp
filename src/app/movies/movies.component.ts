d238473864dfe4a415a67ac78997ec38ae3b870e
Elif Sude Bulut <c1928007@student.cankaya.edu.tr>
2022-07-20 10:52:24 +0200

    4-Template

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'movies',
   templateUrl: 'movies.component.html',
 })
-export class MoviesComponent {}
+export class MoviesComponent {
+  title = 'Movie List';
+
+  movie: Movie = {
+    id: 1,
+    name: 'The Shawshank Redemption ',
+  };
+
+  getTitle() {
+    return this.title;
+  }
+}
